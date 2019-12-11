9dd59e7e40f32773456a78ec8a0049f955fd5d9c
Harry Wilson <harry.wilson1995a@gmail.com>
2019-12-11 14:56:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,18 +23,22 @@ end
 # end 
 
 def assign_rooms(speakers_array)
-  array2 = []
+  array3 = []
   counter = 1 
   speakers_array.each.each_with_index do |name, index|
     room_number = index + 1 
     message = "Hello, #{name}! You'll be assigned to room #{room_number}!"
-    array2 << message 
+    array3 << message 
   end 
-  return array2
+  return array3
 end 
 
 def printer(attendees)
   batch_badge_creator(attendees)
+  array2.each do |name|
+    puts "Hello, my name is #{name}."
   assign_rooms(attendees)
+  array3.each do |name, index|
+    
 end 
   
