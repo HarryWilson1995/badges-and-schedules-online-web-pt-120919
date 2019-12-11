8ef2a229837f13c53a5243c68dbfb035f917a0d0
Harry Wilson <harry.wilson1995a@gmail.com>
2019-12-11 14:47:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,3 +22,13 @@ end
 #   return array2
 # end 
 
+def assign_rooms(speakers_array)
+  array2 = []
+  counter = 1 
+  speakers_array.each.each_with_index do |name, index|
+    room_number = index + 1 
+    message = "Hello, #{name}! You'll be assigned to room #{room_number}!"
+    array2 << message 
+  end 
+  return array2
+end 
