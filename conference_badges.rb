64e1778c52f191c4e1e90d6ac553358b27be0df9
Harry Wilson <harry.wilson1995a@gmail.com>
2019-12-11 15:00:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -33,8 +33,8 @@ def assign_rooms(speakers_array)
   return array3
 end 
 
-def printer(attendees)
-  batch_badge_creator(attendees)
-  assign_rooms(attendees)
-end 
+# def printer(attendees)
+#   batch_badge_creator(attendees)
+#   assign_rooms(attendees)
+# end 
   
