e0d4dce365fc54f0aa85c318a8f4e6b3a94a2356
Harry Wilson <harry.wilson1995a@gmail.com>
2019-12-11 14:34:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,11 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end 
+
+def batch_badge_creator(array)
+  array2 = []
+  array.each do |name|
+    badge = "Hello, my name is #{name}."
+    array2 << badge 
+  end 
+end 
